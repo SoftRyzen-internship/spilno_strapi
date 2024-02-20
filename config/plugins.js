f132e6d8eac46a6c9f8c9ba6946adcc04ff5001c
Svitlana Zhovanik <lanasvetcat@gmail.com>
2024-02-20 15:06:35 +0200

    add vercel plugin

--- config/plugins.js
+++ config/plugins.js
@@ -22,5 +22,16 @@ module.exports = ({ env }) => ({
       defaultLimit: 100,
     },
   },
+   "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook:
+        env('VERCEL_DEPLOY_HOOK'),
+      apiToken: env('VERCEL_API_TOKEN'),
+      appFilter: env('VERCEL_APP_ID'),
+      teamFilter: env('VERCEL_TEAM_ID'),
+      roles: ["strapi-super-admin", "strapi-editor"],
+    },
+  },
   // ...
 });
